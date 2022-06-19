9988eab5f8aa36cb92a6d9ea1bccadc1b8b502d1
Peter Christensen <tessercat@pakaa.net>
2022-06-18 21:07:57 -0600

    Fix a few basics.

--- app.py
+++ app.py
@@ -9,18 +9,25 @@ from starlette.routing import Route
 
 
 logger = logging.getLogger("uvicorn.error")
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-tpl_path = os.path.join(base_dir, "template.html")
-with open(tpl_path, encoding="utf-8") as tpl_fd:
-    template = Template(tpl_fd)
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_verto_template():
+    """ Load the verto template """
+    tpl_path = os.path.join(base_dir, "html", "verto.html")
+    with open(tpl_path, encoding="utf-8") as tpl_fd:
+        return Template(tpl_fd.read())
+
+
+verto_template = get_verto_template()
 
 
 async def verto(request):
     """ Serve the verto call page. """
-    callee = request.query_params.get("call")
+    callee = request.query_params.get("callee")
     if not callee:
         raise HTTPException(400, detail="Missing callee")
-    html = template.substitute(callee=callee)
+    html = verto_template.substitute({"callee": callee})
     return HTMLResponse(html)
 
 
